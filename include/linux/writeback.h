4c17ab143a05eff2ed4b0244fcdc75b4c86ea86b
Psych Half <psych.half@gmail.com>
2016-02-05 21:50:25 +0000

    io_tweaks: add dynamic management of dirty page wtitebacks

--- include/linux/writeback.h
+++ include/linux/writeback.h
@@ -105,6 +105,11 @@ extern unsigned int dirty_expire_interval;
 extern int vm_highmem_is_dirtyable;
 extern int block_dump;
 extern int laptop_mode;
+#ifdef CONFIG_DYNAMIC_PAGE_WRITEBACK
+extern int dyn_dirty_writeback_enabled;
+extern unsigned int dirty_writeback_active_interval;
+extern unsigned int dirty_writeback_suspend_interval;
+#endif
 
 extern unsigned long determine_dirtyable_memory(void);
 
@@ -124,6 +129,16 @@ extern int dirty_bytes_handler(struct ctl_table *table, int write,
 struct ctl_table;
 int dirty_writeback_centisecs_handler(struct ctl_table *, int,
 				      void __user *, size_t *, loff_t *);
+
+#ifdef CONFIG_DYNAMIC_PAGE_WRITEBACK
+int dynamic_dirty_writeback_handler(struct ctl_table *, int,
+              void __user *, size_t *, loff_t *);
+int dirty_writeback_active_centisecs_handler(struct ctl_table *, int,
+              void __user *, size_t *, loff_t *);
+int dirty_writeback_suspend_centisecs_handler(struct ctl_table *, int,
+              void __user *, size_t *, loff_t *);
+#endif
+
 
 void global_dirty_limits(unsigned long *pbackground, unsigned long *pdirty);
 unsigned long bdi_dirty_limit(struct backing_dev_info *bdi,
