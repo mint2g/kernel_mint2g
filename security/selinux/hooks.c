0fae5b1632ea23e705bf234149f2bda70aca83fb
Psych Half <psych.half@gmail.com>
2016-02-05 20:26:25 +0000

    selinux: be permissive by modifying files for now
    passing kernel paramters from command line is not working
    modify selinux to start in permissive
    and prevent changing to enforcing from selinuxfs

--- security/selinux/hooks.c
+++ security/selinux/hooks.c
@@ -105,7 +105,8 @@ static int __init enforcing_setup(char *str)
 {
 	unsigned long enforcing;
 	if (!strict_strtoul(str, 0, &enforcing))
-		selinux_enforcing = enforcing ? 1 : 0;
+		/* be permissive for now -psych.half */
+		selinux_enforcing = 0;
 	return 1;
 }
 __setup("enforcing=", enforcing_setup);
