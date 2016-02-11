06d64553c71d829086de6f94a6a3e8dc15016d3a
Coldwindofnowhere <thereaperwillkillyousoon@gmail.com>
2016-02-11 14:50:06 +0100

    ksm: backport to 3.0.y

--- mm/ksm.c
+++ mm/ksm.c
@@ -1723,10 +1723,8 @@ static void ksm_do_scan(unsigned int scan_npages)
 		rmap_item = scan_get_next_rmap_item(&page);
 		if (!rmap_item)
 			return;
-		if (!PageKsm(page) || !in_stable_tree(rmap_item)) {
-			if (!is_page_scanned(page))
-				cmp_and_merge_page(page, rmap_item);
-		}
+		if (!is_page_scanned(page))
+			cmp_and_merge_page(page, rmap_item);
 		put_page(page);
 	}
 }
